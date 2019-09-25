d02f220a87a7ca4aca8e61d75cd0a530036478cd
atirak <59160624@go.buu.ac.th>
2019-09-25 17:51:29 +0700

    Add Save State

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -28,6 +28,10 @@ import androidx.databinding.DataBindingUtil
 import com.example.android.dessertclicker.databinding.ActivityMainBinding
 import timber.log.Timber
 
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLD = "dessert_sold_key"
+const val KEY_TIMER_SECONDS = "timer_seconds_key"
+
 class MainActivity : AppCompatActivity() {
 
     private var revenue = 0
@@ -167,7 +171,11 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
         Timber.i("onPause Called")
     }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
 
+        Timber.i("onSaveInstanceState Called")
+    }
     override fun onStop() {
         super.onStop()
 //        dessertTimer.stopTimer()
